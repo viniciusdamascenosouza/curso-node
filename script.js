3a87c8bac0c44ce2ddabee83c10dff85bb0dd9ff
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-01-26 15:01:48 -0300

    Rodando api com projeto no Vite

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+import axios from "axios";
+
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
   try {
     const busca = await axios.get("http://localhost:3000/videos");
 
-    const videos = await busca.json();
+    const videos = busca.data;
 
     videos.forEach((video) => {
       if (video.categoria == "") {
@@ -76,6 +78,3 @@ function filtrarPorCategoria(filtro) {
     }
   }
 }
-
-// variável que não existe, para testar eslint
-// console.log(flamengo);
