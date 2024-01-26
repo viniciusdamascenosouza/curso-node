8854e9bee7be1e4c51591f8dbbf27f96e151427e
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-01-26 15:59:40 -0300

    Criando uma prévia de produção

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
   try {
-    const busca = await axios.get("http://localhost:3000/videos");
+    const busca = await axios.get(
+      "https://gist.githubusercontent.com/antonio-evaldo/e8a63621b51c883931eb3fa3a3eca990/raw/12f5c46ee6dd00d03c051adadaf341e06452cea0/videos.txt"
+    );
 
     const videos = busca.data;
 
